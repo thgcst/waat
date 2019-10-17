6b827c980de633040fff81a070c21da388b2c85d
Thiago Costa <44840709+thgcst@users.noreply.github.com>
2019-10-16 23:30:46 -0300

    Mais umas brincadeiras

--- controler.py
+++ controler.py
@@ -45,6 +45,54 @@ def delete(table, where):
     cursor.execute(query)
     con.commit()
 
+def limpa_cpf(cpf):
+    '''Limpa o CPF, tirando ponto e traço'''
+    first = cpf[:3]
+    second = cpf[4:7]
+    third = cpf[8:11]
+    final =cpf[12:]
+    return first+second+third+final
+
+def valida_cpf(cpf):
+    '''Faz a validação do cpf inserido. Retorna True se for válido e False se for inválido'''
+    cpf_limpo = limpa_cpf(cpf)
+
+    digitos=[]
+    for i in range (0, 11):
+        digitos.append(int(cpf_limpo[i]))
+    
+    primeiro_validador = 0
+    for i in range (0, 9):
+        buffer = (10-i)*digitos[i]
+        primeiro_validador += buffer
+    primeiro_resto = primeiro_validador%11    
+
+    if primeiro_resto<2:
+        primeiro_digito = 0
+    else:
+        primeiro_digito = 11-primeiro_resto
+    
+    if primeiro_digito != digitos[9]:
+        return False
+
+    segundo_validador=0
+
+    for i in range (0, 10):
+        buffer = (11-i)*digitos[i]
+        segundo_validador += buffer
+    segundo_resto = segundo_validador%11
+
+    if segundo_resto<2:
+        segundo_digito=0
+    else:
+        segundo_digito = 11-segundo_resto
+
+    if segundo_digito != digitos[10]:
+        return False
+
+    return True
+
+
 def verifica_cpf(cpf, tabela):
     """retorna True se o cpf já está cadastrado e False c.c."""
     cpf = "cpf="+str(cpf)
@@ -79,16 +127,17 @@ def gera_id_profissional():
     return id_gerado
 
 
-def cadastra_cliente(nome, data_de_nascimento, cpf, telefone, endereco, email, senha, cpf_responsavel, nome_responsavel):
+def cadastra_cliente(nome, data_de_nascimento, cpf, telefone, email, senha, cep, endereco, numero, complemento, cidade, estado, nome_responsavel, cpf_responsavel):
     id_cliente = gera_id_cliente()
-    sql = "INSERT INTO clientes (id_cliente, nome, data_de_nascimento, cpf, telefone, endereco, email, senha, cpf_responsavel, nome_responsavel) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    data = (id_cliente, nome, data_de_nascimento, cpf, telefone, endereco, email, senha, cpf_responsavel, nome_responsavel)
+    sql = "INSERT INTO clientes (id_cliente, nome, data_de_nascimento, cpf, telefone, email, senha, cep, endereco, numero, complemento, cidade, estado, nome_responsavel, cpf_responsavel) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    data = (id_cliente, nome, data_de_nascimento, cpf, telefone, email, senha, cep, endereco, numero, complemento, cidade, estado, nome_responsavel, cpf_responsavel)
     cursor.execute(sql, data)
     con.commit()
-#em progresso
-def cadastra_profissional(nome, cpf, profissao, endereco_comercial, email, registro_profissional, telefone, senha):
+
+
+def cadastra_profissional(nome, cpf, profissao, registro_profissional, telefone, data_de_nascimento, email, senha, cep, endereco, numero, complemento, cidade, estado):
     id_profissional = gera_id_profissional()
-    sql = "INSERT INTO profissionais (id_profissional, nome, cpf, profissao, endereco_comercial, email, registro_profissional, telefone, senha) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    data = (id_profissional, nome, cpf, profissao, endereco_comercial, email, registro_profissional, telefone, senha)
+    sql = "INSERT INTO profissionais (id_profissional, nome, cpf, profissao, registro_profissional, telefone, data_de_nascimento, email, senha, cep, endereco, numero, complemento, cidade, estado) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    data = (id_profissional, nome, cpf, profissao, registro_profissional, telefone, data_de_nascimento, email, senha, cep, endereco, numero, complemento, cidade, estado)
     cursor.execute(sql, data)
     con.commit()
